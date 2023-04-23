7628b4d75083fc854ba257d13365b4ca43323022
MishaShapiro <112972631+MishaShapiro@users.noreply.github.com>
2023-04-24 00:42:03 +0300

    Delete Task5.js

--- SkillFactory/Frontend PRO/JavaScript/Task5.js
+++
@@ -1,7 +0,0 @@
-var arr = [6, 3, 9, 1];
-
-console.log("Количество элементов в массиве: " + arr.length)
-
-let result = arr.map(function(item, index, array) {
-	console.log(`Элемент массива с индексом ${index}: ` + item)
-});
