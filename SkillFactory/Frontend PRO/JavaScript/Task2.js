94851e0b660dfebca35aae5bb5060e7b32763db9
MishaShapiro <112972631+MishaShapiro@users.noreply.github.com>
2023-04-24 00:41:48 +0300

    Delete Task2.js

--- SkillFactory/Frontend PRO/JavaScript/Task2.js
+++
@@ -1,9 +0,0 @@
-let x = "sdf"
-
-if (typeof x == "number") {
-  console.log("x - число")
-} else if (typeof x == "boolean") {
-  console.log("x - логический тип")
-} else {
-  console.log("Тип x не определён")
-}
